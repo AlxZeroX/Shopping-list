3250dc66a8ce670411b7ac1aba9d83d7422b4ea2
Cristian_Martinez <thealxzerox@gmail.com>
2021-12-04 17:37:01 -0600

    item component

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/item';
 
 @Component({
   selector: 'app-items',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsComponent implements OnInit {
 
+  items: Item[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.items = [
+
+      {
+        id: 0,
+        title: 'pera',
+        price: 12.5,
+        quantity: 5,
+        completed: false
+      },
+
+      {
+        id: 1,
+        title: 'aguacate',
+        price: 10,
+        quantity: 3,
+        completed: true
+      },
+
+
+    ]
   }
 
 }
